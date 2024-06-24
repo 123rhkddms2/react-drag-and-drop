51c511e2a4f1b277fc1843d1e505720610bd2f6b
123rhkddms2 <123rhkddms2@gmail.com>
2024-06-24 10:12:40 +0900

    react 드래그앤드롭

---
+++ README.md
@@ -0,0 +1,10 @@
+# React-drag-and-drop
+리액트 Drag and Drop 기능 실습
+
+- 패키지 설치
+   - npm install
+   - npm install react-beautiful-dnd
+   - npm install sass   
+
+- 주의사항 
+   - Draggable 기능을 위해 index.js에서 <React.StrictMode> 주석해야 됨
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "./App.css";
+import Basic from "./components/Basic";
+import Multi from "./components/Multi";
+
+/*
+  날짜 : 2024/05/24
+  이름 : 김광은
+  내용 : 리액트 Drag and Drop 기능 실습
+  
+  패키지 설치
+   - npm install react-beautiful-dnd
+   - npm install sass 
+
+  주의사항 
+   - Draggable 기능을 위해 index.js에서 <React.StrictMode> 주석해야 됨
+*/
+const App = () => {
+  return (
+    <div className="App">
+      <h3>기본</h3>
+      <Basic />
+
+      <h3>확장</h3>
+      <Multi />
+    </div>
+  );
+};
+
+export default App;
